cd817f25330870e87b08f6cafac2d53801e4b1a2
PandaWebDevX <aslamansari5207@gmail.com>
2024-08-22 00:19:16 +0530

    modified:   aslam.js 	modified:   index.html 	modified:   responsive.css

--- aslam.js
+++ aslam.js
@@ -1,10 +1,14 @@
 const ham= document.querySelector('.ham');
 const nav =document.querySelector('.navigation');
 const logo =document.querySelector('.logo');
+const tem =document.querySelector('.template');
+const body =document.querySelector('.body');
 ham.addEventListener('click',()=>{
     ham.classList.toggle('active');
     nav.classList.toggle('active');
     logo.classList.toggle('active');
+    tem.classList.toggle('active');
+    body.classList.toggle('active');
 
    
 
