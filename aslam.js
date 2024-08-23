2c931526c3fb4b183e57aaca516ad42034f1b590
PandaWebDevX <aslamansari5207@gmail.com>
2024-08-23 16:40:12 +0530

    modified:   aslam.js 	modified:   mobile.css

--- aslam.js
+++ aslam.js
@@ -4,6 +4,8 @@ const barm = document.querySelector('.bar-2');
 const barl = document.querySelector('.bar-3');
 const nav =document.querySelector('.nav');
 const slide =document.querySelector('.slide');
+const icons =document.querySelector('.icons');
+const logo=document.querySelector('.logo');
 
 ham.addEventListener('click',()=>{
     barf.classList.toggle('active');
@@ -11,6 +13,8 @@ ham.addEventListener('click',()=>{
     barl.classList.toggle('active');
     ham.classList.toggle('active');
     nav.classList.toggle('active');
+    icons.classList.toggle('active');
+    logo.classList.toggle('active');
     slide.classList.toggle('active');
 
 })
